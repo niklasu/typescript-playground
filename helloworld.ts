601b329b97732c7a612adc728a17a7bd795d5e18
Niklas Enns <niklas.enns@consol.de>
2020-12-07 22:32:56 +0100

    hello typescript

---
+++ helloworld.ts
@@ -0,0 +1,7 @@
+let message: string = 'Hello World!';
+console.log(message);
+
+console.log("Ich mag TypeSscript :-))")
+
+let a = 2
+console.log(`Juhuu, endlich keine Semikolons mehr :-). a = ${a}`)
