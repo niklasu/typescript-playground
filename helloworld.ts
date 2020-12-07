9da48f1d1f97a49fdd841f1ff4d550ba523a8b16
Niklas Enns <niklas.enns@consol.de>
2020-12-07 22:38:14 +0100

    fix typo

--- helloworld.ts
+++
@@ -1,7 +0,0 @@
-let message: string = 'Hello World!';
-console.log(message);
-
-console.log("Ich mag TypeSscript :-))")
-
-let a = 2
-console.log(`Juhuu, endlich keine Semikolons mehr :-). a = ${a}`)
