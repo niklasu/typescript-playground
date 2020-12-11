d54c9b764e46364ec996d7d12ba766919b66fad7
Niklas Enns <niklas.enns@consol.de>
2020-12-11 10:29:09 +0100

    destructuring

--- src/__tests__/test.ts
+++ src/__tests__/test.ts
@@ -1,9 +1,17 @@
-test('basic', () => {
-    let a = 0
-    expect(a).toBe(0);
+test('Boolean', () => {
+    let myBool = true
+    expect(myBool).toBeTruthy()
 });
 
 test('basic', () => {
     let a = 1
     expect(a).toBe(0);
+});
+
+test('destructuring', () => {
+    const response = {a: 'a', b: () => {return 'b'}, c: 34}
+    const {a, b} = response;
+    expect(a).toBe('a')
+    expect(b()).toBe('b')
+    //c is not available here
 });
