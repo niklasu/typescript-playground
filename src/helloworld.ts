be8676da5a3ed5391a6b5e59b9fd2fb64ccc6c15
Niklas Enns <niklas.enns@gmail.com>
2020-12-16 22:30:38 +0100

    rearranged logs

--- src/helloworld.ts
+++ src/helloworld.ts
@@ -1,7 +1,8 @@
 import { Logger } from "tslog";
 
 const log: Logger = new Logger();
-log.info("I am a info log.");
-log.error("I am a error log.");
 log.fatal("I am a fatal log.");
+log.error("I am a error log.");
+log.info("I am a info log.");
 log.debug("I am a silly log.");
+log.silly("I am a silly log.");
