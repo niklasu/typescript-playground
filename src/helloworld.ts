f77811e21792724474e4c64a6fbc8750d9fd0e8b
Niklas Enns <niklas.enns@gmail.com>
2020-12-16 22:31:28 +0100

    rearranged logs

--- src/helloworld.ts
+++ src/helloworld.ts
@@ -5,4 +5,5 @@ log.fatal("I am a fatal log.");
 log.error("I am a error log.");
 log.info("I am a info log.");
 log.debug("I am a silly log.");
+log.warn("I am an error log.");
 log.silly("I am a silly log.");
